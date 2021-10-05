f3698d4ec07d72543f5b21ad159f8830f2889132
Aymane Talibi <aymane.talibi.at@gmail.com>
2021-10-05 04:46:02 +0100

    start the project
    done

---
+++ src/main/java/com/talibi/emsAng/EmployeManagementMiniAppBaackendApplication.java
@@ -0,0 +1,13 @@
+package com.talibi.emsAng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeManagementMiniAppBaackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeManagementMiniAppBaackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/talibi/emsAng/EmployeManagementMiniAppBaackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.talibi.emsAng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeManagementMiniAppBaackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
