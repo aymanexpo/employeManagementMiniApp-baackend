45aeaf3500ba5681f644182e86b2d0bc7993b607
Aymane Talibi <aymane.talibi.at@gmail.com>
2021-10-12 20:50:16 +0100

    create front end component
    en cours

--- frontend side/ems-fronted/src/app/app-routing.module.ts
+++ frontend side/ems-fronted/src/app/app-routing.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { CreateEmployeeComponent } from './create-employee/create-employee.component';
 
 const routes: Routes = [
   {path: 'employees', component: EmployeeListComponent},
-  {path: '', redirectTo: 'employees', pathMatch: 'full'}
+  {path: 'create-employee', component: CreateEmployeeComponent},
+  {path: '', redirectTo: 'employees', pathMatch: 'full'},
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes)],                                                                                                                                                                                                                                                                                                          
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
---
+++ frontend side/ems-fronted/src/app/create-employee/create-employee.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Employee } from '../employee';
+
+@Component({
+  selector: 'app-create-employee',
+  templateUrl: './create-employee.component.html',
+  styleUrls: ['./create-employee.component.css']
+})
+export class CreateEmployeeComponent implements OnInit {
+
+  employee: Employee = new Employee();
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit(){
+
+  }
+
+}
