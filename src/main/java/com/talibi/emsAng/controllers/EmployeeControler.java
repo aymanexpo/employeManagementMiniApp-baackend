1f1d75f48880e04f0192dccd7281e758e16d903a
Aymane Talibi <aymane.talibi.at@gmail.com>
2021-10-14 17:11:19 +0100

    Getting employees by id rest api
    done

--- src/main/java/com/talibi/emsAng/controllers/EmployeeControler.java
+++ src/main/java/com/talibi/emsAng/controllers/EmployeeControler.java
@@ -3,13 +3,16 @@ package com.talibi.emsAng.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.talibi.emsAng.exceptions.ResourceNotFoundException;
 import com.talibi.emsAng.models.Employee;
 import com.talibi.emsAng.repositories.EmployeeRepository;
 
@@ -29,5 +32,12 @@ public class EmployeeControler {
 	@PostMapping("/employees")
 	public Employee createEmployee(@RequestBody Employee employee) {
 		return employeeRepository.save(employee);
+	}
+	
+	@GetMapping("/employees/{id}")
+	public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id) {
+		Employee employee = employeeRepository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("employee don't exist with id :" + id));
+		return ResponseEntity.ok(employee);
 	}
 }
