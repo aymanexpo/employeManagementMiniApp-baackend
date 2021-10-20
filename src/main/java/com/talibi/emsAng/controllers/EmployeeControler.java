b0ee41ce1690728b1c037ba832d17824d7f3a8fe
Aymane Talibi <aymane.talibi.at@gmail.com>
2021-10-20 03:30:12 +0100

    Delete REST API
    done
    
    last step p 1

--- src/main/java/com/talibi/emsAng/controllers/EmployeeControler.java
+++ src/main/java/com/talibi/emsAng/controllers/EmployeeControler.java
@@ -1,10 +1,13 @@
 package com.talibi.emsAng.controllers;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -53,5 +56,16 @@ public class EmployeeControler {
 		
 		Employee updateEmployee = employeeRepository.save(employee);
 		return ResponseEntity.ok(updateEmployee);
+	}
+	
+	@DeleteMapping("/employees/{id}")
+	public ResponseEntity <Map<String, Boolean>> deleteEmployee(@PathVariable Long id){
+		Employee employee = employeeRepository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Employee don't exist with id :" +id));
+		
+		employeeRepository.delete(employee);
+		Map<String, Boolean> response =new HashMap<>();
+		response.put("deleted", Boolean.TRUE);
+		return ResponseEntity.ok(response);
 	}
 }
