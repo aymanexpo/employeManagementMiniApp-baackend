1042f72a3040fc10c590cc09db7227c777cfc363
Aymane Talibi <aymane.talibi.at@gmail.com>
2021-10-21 21:38:25 +0100

    code source finale version
    done + tested the api with PostMan
    tested with navigateur
    success connection

--- frontend side/ems-fronted/src/app/app.module.ts
+++ frontend side/ems-fronted/src/app/app.module.ts
@@ -1,19 +1,21 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EmployeeListComponent } from './employee-list/employee-list.component';
 import { CreateEmployeeComponent } from './create-employee/create-employee.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule} from '@angular/forms';
 import { UpdateEmployeeComponent } from './update-employee/update-employee.component';
+import { EmployeeDetailsComponent } from './employee-details/employee-details.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     EmployeeListComponent,
     CreateEmployeeComponent,
-    UpdateEmployeeComponent
+    UpdateEmployeeComponent,
+    EmployeeDetailsComponent
   ],
   imports: [
     BrowserModule,
--- frontend side/ems-fronted/src/app/employee.service.ts
+++ frontend side/ems-fronted/src/app/employee.service.ts
@@ -9,9 +9,9 @@ import { Employee } from './employee';
 export class EmployeeService {
 
   private baseURL = "http://localhost:8080/api/v1/employees";
-  
-  constructor(private httpClient: HttpClient) { }
 
+  constructor(private httpClient: HttpClient) { }
+  
   getEmployeesList(): Observable<Employee[]>{
     return this.httpClient.get<Employee[]>(`${this.baseURL}`);
   }
